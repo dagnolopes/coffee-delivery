108c820505f84c3c7a3f1549548105e1dbcfc5a5
Dagno Lopes <dagno.lopes2019@gmail.com>
2023-01-26 21:41:22 -0300

    Base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function App() {
+  const [count,
+     setCount] = useState(0)
+
+  return (
+    <h1>Ola mundo</h1>    
+    
+  )
+
+}
+
+export default App
